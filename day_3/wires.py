b6d998abf439e4ae1b7818adf53c7dd7ac3bd82a
Wilson Burchenal <wilson@3dhubs.com>
2020-06-25 14:27:00 +0200

    Day 3 finished

--- day_3/wires.py
+++ day_3/wires.py
@@ -1,55 +1,17 @@
 # Advent of Code 2019 Day 3
 
 
-def manhattan_distance(x_start, y_start, x_index, y_index):
-    return abs(x_index - x_start) + abs(y_index - y_start)
+def run_wire(wire):
 
-
-def calc_intersection_distances(board):
-    min_distance = len(board)
-    for i in range(0, len(board)):
-        for j in range(0, len(board)):
-            if board[i][j] == 2:
-                start = int(len(board)/2)
-                distance = manhattan_distance(start, start, i, j)
-
-                if distance < min_distance:
-                    min_distance = distance
-
-    return min_distance
-
-
-def init_board(size):
-    board = [[0 for i in range(size)] for j in range(size)]
-    return board
-
-
-def print_board_state(board):
-
-    for i in board:
-        print(i)
-
-
-def run_wire(board, wire):
-
-    x_start = y_start = int(len(board)/2)
-    board[x_start][y_start] = 7
+    coordinates = {}
+    x_start = y_start = steps = 0
     x_index = x_start
     y_index = y_start
-    print(wire)
 
     for command in wire:
         direction = command[0:1]
         distance = int(command[1:])
-
         for j in range(0, distance):
-            # print(str(x_index) + ', ' + str(y_index))
-            # print_board_state(board)
-            if board[x_index][y_index] == 1:
-                board[x_index][y_index] = 2
-            elif board[x_index][y_index] == 0:
-                board[x_index][y_index] = 1
-
             if direction == 'R':
                 x_index += 1
             elif direction == 'L':
@@ -59,27 +21,32 @@ def run_wire(board, wire):
             elif direction == 'D':
                 y_index -= 1
 
+            steps += 1
+
+            if (x_index, y_index) not in coordinates:
+                coordinates[(x_index, y_index)] = steps
+
+    return coordinates
+
 
 if __name__ == '__main__':
-
-    board = init_board(20000)
 
     with open('input.txt', 'r') as input:
 
         wire1 = input.readline().split(',')
+        wire1 = [s.strip('\n') for s in wire1]
         wire2 = input.readline().split(',')
+        wire2 = [s.strip('\n') for s in wire2]
 
-        # wire1 = ['R8','U5','L5','D3']
-        # wire2 = ['U7','R6','D4','L4']
+        wire1_coordinates = run_wire(wire1)
+        wire2_coordinates = run_wire(wire2)
 
-        # wire1 = ['R75','D30','R83','U83','L12','D49','R71','U7','L72']
-        # wire2 = ['U62','R66','U55','R34','D71','R55','D58','R83']
+        intersection_coordinates = [coord for coord in wire1_coordinates if coord in wire2_coordinates]
 
-        # wire1 = ['R98','U47','R26','D63','R33','U87','L62','D20','R33','U53','R51']
-        # wire2 = ['U98','R91','D20','R16','D67','R40','U7','R15','U6','R7']
+        # Part 1
+        min_distance = min(abs(x) + abs(y) for (x, y) in intersection_coordinates)
+        print(min_distance)
 
-        run_wire(board, wire1)
-        run_wire(board, wire2)
-        # print_board_state(board)
-
-        print(calc_intersection_distances(board))
+        # Part 2
+        shortest_steps = min(wire1_coordinates[coord] + wire2_coordinates[coord] for coord in intersection_coordinates)
+        print(shortest_steps)
